63264fe2cb6bc29340232028187cc877c975f655
Fulgent Kvasir Lavesores <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2023-10-20 05:10:41 +0800

    features: popup modals

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, Blueprint, redirect
 from routes.student_bp import student_bp
+import mysql.connector
+from db.db_config import db_config
+
 
 app = Flask(__name__)
+
+
+def connect_to_database():
+    return mysql.connector.connect(**db_config)
+
 
 @app.route('/')
 def index():
@@ -13,7 +21,15 @@ def courses():
 
 @app.route('/colleges')
 def colleges():
-    return render_template('college.html', active_page='colleges')
+    conn = connect_to_database()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute('SELECT * FROM college')
+    colleges_data = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
+    return render_template('college.html', active_page='colleges', colleges_data=colleges_data)
 
 app.register_blueprint(student_bp, url_prefix='/students')
 
---
+++ db/db_config.py
@@ -0,0 +1,8 @@
+# db_config.py
+
+db_config = {
+    'host': 'localhost',  
+    'user': 'root',  
+    'password': 'fkvasir1',
+    'database': 'web_ssis'
+}
