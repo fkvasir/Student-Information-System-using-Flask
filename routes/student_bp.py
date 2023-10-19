d2b4faec786f852ba275e6ce9a24c52d7c89ec74
Fulgent Kvasir Lavesores <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2023-10-19 12:53:59 +0800

    changing styles: buttons

--- routes/student_bp.py
+++ routes/student_bp.py
@@ -4,13 +4,13 @@ student_bp = Blueprint('student', __name__, template_folder='templates')
 
 @student_bp.route('/')
 def students():
-    return render_template('students.html', active_page='student')
+    return render_template('students.html', active_page='students')
 
 @student_bp.route('/colleges')
 def colleges():
-    return render_template('college.html', active_page='college')
+    return render_template('college.html', active_page='colleges')
 
-@student_bp.route('/colleges')
+@student_bp.route('/courses')
 def courses():
     return render_template('courses.html', active_page='courses')
 
