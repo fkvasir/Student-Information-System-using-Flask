0f16448dd5676199e37d13ad4cbfd0089a51e69c
Fulgent Kvasir Lavesores <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2023-10-23 17:00:34 +0800

    features::modals

--- routes/colleges_bp.py
+++ routes/colleges_bp.py
@@ -1,13 +1,13 @@
 from flask import render_template, Blueprint, redirect, url_for
 from models.colleges_m import get_colleges
 
-colleges_bp = Blueprint('colleges_bp', __name__,template_folder='templates')
+colleges_bp = Blueprint('colleges_bp', __name__, template_folder='templates')
 
 @colleges_bp.route('/')
 def colleges_default():
     return redirect(url_for('colleges_bp.colleges_route'))
 
-@colleges_bp.route('/colleges-list')
+@colleges_bp.route('/colleges')
 def colleges_route():
     colleges_data = get_colleges()
     return render_template('college.html', active_page='colleges', colleges_data=colleges_data)
