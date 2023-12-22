6efd4d99448df94486a731254fe5936defd2c61d
Fulgent Kvasir Lavesores <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2023-12-22 11:34:01 +0800

    Fixed Changes

--- app/models/collegeModel.py
+++ app/models/collegeModel.py
@@ -2,12 +2,12 @@ from app import mysql
 
 class College:
     @staticmethod
-    def get_by_collegecode(college_code):
+    def get_by_collegeName(college_name):
         connection = mysql.connection
         cursor = connection.cursor(dictionary=True)
 
-        query = "SELECT * FROM college WHERE collegeCode = %s"
-        cursor.execute(query, (college_code,))
+        query = "SELECT * FROM college WHERE collegeName = %s"
+        cursor.execute(query, (college_name,))
         college_data = cursor.fetchone()
 
         cursor.close()
